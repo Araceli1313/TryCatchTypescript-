09f68110442b3ab78fd2129553ff6867270dda5b
Araceli de los Angeles Asencio <araceli.asencio@ssasa.com>
2024-05-24 16:44:25 -0600

    :construction_worker: Modificaciones

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+/*
+    try {
+        Código que puede fallar 
+    } catch (error) {
+        Qué hacer cuando ese código fallé 
+    }
+*/
+
+/*function dividirNumeros (a: number, b: number) {
+  
+    
+    try {
+         if (b === 0)
+            throw new Error ('No es posible dividir por 0   ')
+        return a / b
+    } catch (error) {
+      
+        if  (error instanceof Error) console.log('Error: ', error.message)
+        return -1
+    }
+
+}
+
+*/
